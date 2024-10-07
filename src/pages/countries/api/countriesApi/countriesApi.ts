854ab20efc77f5cd1799fda3cc806dd63ccca9f6
richi1213 <datonanava11@gmail.com>
2024-10-07 15:17:26 +0400

    fixing fetching cold countries

--- src/pages/countries/api/countriesApi/countriesApi.ts
+++ src/pages/countries/api/countriesApi/countriesApi.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_REST_COUNTRIES_URL;
+type CountryApiResponse = {
+  name: { common: string };
+  flags: { png: string };
+  population: number;
+  capital: string[];
+};
 
 export type CountryData = {
   name: string;
@@ -9,11 +14,15 @@ export type CountryData = {
   capital: string;
 };
 
+const BASE_URL = import.meta.env.VITE_REST_COUNTRIES_URL;
+
 export const fetchColdCountryData = async (
   country: string
 ): Promise<CountryData | null> => {
   try {
-    const response = await axios.get(`${BASE_URL}${country}?fullText=true`);
+    const response = await axios.get<CountryApiResponse[]>(
+      `${BASE_URL}${country}?fullText=true`
+    );
 
     if (!Array.isArray(response.data) || response.data.length === 0) {
       throw new Error("No data found for the specified country.");
@@ -27,8 +36,14 @@ export const fetchColdCountryData = async (
       population: population ?? 0,
       capital: capital?.[0] ?? "N/A",
     };
-  } catch (error) {
-    console.error(`Error fetching data for ${country}:`, error);
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        `Error fetching data for ${country}: ${error.response?.status}`
+      );
+    } else {
+      console.error(`Error fetching data for ${country}:`, error.message);
+    }
     return null;
   }
 };
