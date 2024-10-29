26e60dd05edabf319b87fe8ce93c689c6c1f5578
richi1213 <datonanava11@gmail.com>
2024-10-29 10:36:23 +0400

    .

--- eslint.config.js
+++ eslint.config.js
@@ -2,35 +2,23 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        process: true,
-        React: 'readonly',
-        JSX: 'readonly',
-      },
-      parser: tsParser,
-      parserOptions: {
-        project: ['./tsconfig.eslint.json'],
-      },
+      globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tseslint,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
@@ -38,4 +26,4 @@ export default [
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-];
+);
